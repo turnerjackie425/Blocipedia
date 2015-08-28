c5656eec4825b8e20120dad276ed75898bc5392d
Jackie Turner <turnerjackie425@gmail.com>
2015-08-28 15:53:03 -0600

    errors still

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -16,7 +16,7 @@ class Scope
        elsif user.role == 'premium'
          all_wikis = scope.all
          all_wikis.each do |wiki|
-           if wiki.public? || wiki.user == user || wiki.users.include?(user)
+           if !wiki.private? || wiki.user == user || wiki.users.include?(user)
              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
            end
          end
